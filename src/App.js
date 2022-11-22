06994dde4616caa8494f91ba468d450d087e868d
Colin Foulkes <cfoulkes@protegra.com>
2022-11-22 07:14:34 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { useState } from 'react';
 import './App.css';
 import Content from './components/content';
-import Header from './components/header';
+// import InnerContent from './components/inner-content';
+// import Header from './components/header';
 
 function App() {
   const [shrunk, setShrunk] = useState(false);
 
   const handleScroll = event => {
     var scrollTop = event.currentTarget.scrollTop;
-    console.log('scrollTop: ', event.currentTarget.scrollTop);
-//    console.log('offsetHeight: ', event.currentTarget.offsetHeight);
+    // console.log('scrollTop: ', event.currentTarget.scrollTop);
+    // console.log('offsetHeight: ', event.currentTarget.offsetHeight);
 
     setShrunk(scrollTop > 5)
   };
@@ -17,9 +18,9 @@ function App() {
   return (
     <div className="App">
       {/* <div className={`header ${shrunk ? "minimized" : ""}`}><Header minimize={shrunk}/></div> */}
-      <div className={`header ${shrunk ? "minimized" : ""}`}>header</div>
-      <div className={`content ${shrunk ? "maximized" : ""}`} onScroll={handleScroll}><Content/></div>
-      <div className='footer'>footer</div>
+      <div className={`main-header ${shrunk ? "minimized" : ""}`}>header</div>
+      <div className={`main-content ${shrunk ? "maximized" : ""}`} onScroll={handleScroll}><Content/></div>
+      <div className='main-footer'>footer</div>
     </div>
   );
 }
--- src/components/content.js
+++ src/components/content.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './content.css';
+import InnerContent from './inner-content'
 
 function Content() {
+
+  const [scrolling, setScrolling] = useState(false);
+
+  const handleScroll = event => {
+    var scrollTop = event.currentTarget.scrollTop;
+    // console.log('scrollTop: ', event.currentTarget.scrollTop);
+    // console.log('offsetHeight: ', event.currentTarget.offsetHeight);
+    setScrolling(scrollTop > 5)
+  };
+
   return (
-    (() => {
-        let divs = [];
-        for (var i = 0; i < 100; i++) {
-            divs.push(<div>{`we are at row ${i}`}</div>)
-        }
-        return divs
-    })()
+    <div className='content'>
+      <div className={`header ${scrolling ? "minimized" : ""}`}>Inner Header</div>
+      <div className='body' onScroll={handleScroll}><InnerContent/></div>
+      {/* <div className='body'>Some body</div> */}
+      <div className='footer'>Inner Footer</div>
+    </div>
   )
 }
 
